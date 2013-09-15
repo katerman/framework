7729995bce8a616d2cd6e73128531fc7b433f677
Kevin Reynolds <katermanlw@gmail.com>
2013-09-15 02:14:22 -0400

    updated model view to use more sql step towards dynamic pages
    yay.

--- AuthModel.php
+++ AuthModel.php
@@ -2,15 +2,14 @@
 
 class AuthModel{
 
-	public $db;
+	static $db;
 	
 	public function __construct($dsn, $user, $pass){
 		try{
-		$this->db = new PDO($dsn, $user, $pass);
-		
-		}catch (PDOException $e){
-			
-			var_dump($e);
+			$this->db = new PDO($dsn, $user, $pass);
+				
+		} catch (PDOException $e) {
+			var_dump($e);		
 		}
 		//$this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 	}
@@ -29,13 +28,27 @@ class AuthModel{
 				return $rows[0];
 			}
 		}
-		return FALSE;
-	}//end of get users
+		return array();//If the expected successful return is an array, the failed return should be an empty array
+	}//end of get users.
 	
-	
-	
-	
-	
+	public function sqlPages(){
+		//die(print_r(debug_backtrace(),true));
+		$stmt = $this->db->prepare("SELECT page_id, page_name FROM pages");
+		try {
+			if ($stmt->execute()) {
+				$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			}
+		}
+		catch(PDOException $e){
+			echo "Query Failed - users";
+		}	
+
+		print_r($results);
+		
+		foreach($results as $key=>$r){
+			echo '<p>'. $r[page_name] .'</p>';
+		}
+	}
 	
 }
 
